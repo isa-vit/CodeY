3906df0f215440fa14c9cf9cfa989b15bfb23658
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-10-07 10:36:43 +0530

    Basic Structure modification

---
+++ CodeY/app/src/main/java/com/isavit/codey/home/Notes.kt
@@ -0,0 +1,12 @@
+package com.isavit.codey.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.isavit.codey.R
+
+class Notes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes)
+    }
+}
