3f1cd80a1541891a4e7c8b0dae98ce2cf8a13389
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-10-07 09:53:36 +0530

    Algorithm class structure

---
+++ CodeY/app/src/main/java/com/isavit/codey/security/Algorithm.java
@@ -0,0 +1,25 @@
+package com.isavit.codey.security;
+
+public class Algorithm {
+
+    private String key = "";
+
+    public static String encryption(String plainText) {
+
+        String encryptedText = "";
+
+
+        return encryptedText;
+
+    }
+
+    public static String decryption(String cipherText) {
+
+        String decryptedText = "";
+
+
+        return decryptedText;
+
+    }
+
+}
