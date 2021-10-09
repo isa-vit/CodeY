4dd4a74302cf0d476f5c4ae8edb85be4f2bf454b
rudrankbasant <rudrank123@gmail.com>
2021-10-09 18:49:47 +0530

    "Sign in with password" takes to login page.

--- CodeY/app/src/main/java/com/isavit/codey/authentication/Signup.java
+++ CodeY/app/src/main/java/com/isavit/codey/authentication/Signup.java
@@ -3,6 +3,8 @@ package com.isavit.codey.authentication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 import com.isavit.codey.R;
 
@@ -20,5 +22,9 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+    public void taketoLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
