8bf08a2a080aa0f88a55c579798847eba13ebfd9
rudrankbasant <rudrank123@gmail.com>
2021-10-10 16:50:40 +0530

    ISSUE #12 SQLite DB for Username, Password #12

--- CodeY/app/src/main/java/com/isavit/codey/authentication/Login.java
+++ CodeY/app/src/main/java/com/isavit/codey/authentication/Login.java
@@ -6,7 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.isavit.codey.R;
+import com.isavit.codey.home.Home;
+
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  *
@@ -16,17 +22,49 @@ import android.view.View;
  */
 
 public class Login extends AppCompatActivity {
+    EditText username, password;
+    Button btnLogin, gotoSignUp;
+    DBHelper myDB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username=(EditText)findViewById(R.id.usernameLogin);
+        password=(EditText)findViewById(R.id.passwordLogin);
+        btnLogin=(Button)findViewById(R.id.btnLogin);
+        gotoSignUp=(Button)findViewById(R.id.gotoSignUp);
+
+        myDB = new DBHelper(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals("")){
+                    Toast.makeText(Login.this,"Please fill both Username and Password!",Toast.LENGTH_SHORT).show();
+                }else{
+                    boolean result = myDB.checkusernamePassword(user, pass);
+                    if(result==true){
+                        Intent intent = new Intent(getApplicationContext(), Home.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(Login.this,"Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+        gotoSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Signup.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void taketoSignup(View view) {
-        Intent intent = new Intent(this, Signup.class);
-        startActivity(intent);
-    }
-
-
-
 }
--- CodeY/app/src/main/java/com/isavit/codey/authentication/Signup.java
+++ CodeY/app/src/main/java/com/isavit/codey/authentication/Signup.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.isavit.codey.R;
 import com.isavit.codey.home.Home;
@@ -18,18 +21,62 @@ import com.isavit.codey.home.Home;
  */
 
 public class Signup extends AppCompatActivity {
+    EditText username, password, repassword;
+    Button btnSignUp, gotoSignIn;
+    DBHelper myDB;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-    }
-    public void taketoLogin(View view) {
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
-    }
-    public void deletethis(View view) {
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
+
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        repassword=(EditText)findViewById(R.id.repassword);
+
+        btnSignUp=(Button)findViewById(R.id.btnSignUp);
+        gotoSignIn=(Button)findViewById(R.id.gotoSignIn);
+        myDB = new DBHelper(this);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass= repassword.getText().toString();
+
+                if(user.equals("")|| pass.equals("") || repass.equals("")){
+                    Toast.makeText(Signup.this, "Fill all fields",Toast.LENGTH_SHORT).show();
+                }else{
+                    if(pass.equals(repass)){
+                       boolean usercheckResult= myDB.checkusername(user);
+                       if(usercheckResult==false){
+                           boolean regResult= myDB.insertData(user, pass);
+                           if(regResult==true){
+                               Toast.makeText(Signup.this, "Registered Successfully.",Toast.LENGTH_SHORT).show();
+                               Intent intent = new Intent (getApplicationContext(), Login.class) ;
+                               startActivity(intent);
+                           }else{
+                               Toast.makeText(Signup.this, "Registration Failed!",Toast.LENGTH_SHORT).show();
+                           }
+                       }else{
+                           Toast.makeText(Signup.this, "User already exists. \n Please Sign In.",Toast.LENGTH_SHORT).show();
+                       }
+
+                    }else{
+                        Toast.makeText(Signup.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        gotoSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
