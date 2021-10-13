8e366853057e27b1b4fe6ec157508318ae7ce3c8
= <mailofgarv@gmail.com>
2021-10-14 03:08:46 +0530

    Added Persistent theme switch method and button added on activity_home.xml

--- CodeY/app/src/main/java/com/isavit/codey/home/Home.kt
+++ CodeY/app/src/main/java/com/isavit/codey/home/Home.kt
@@ -3,17 +3,25 @@ package com.isavit.codey.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import com.isavit.codey.R
+import com.isavit.codey.home.MyPrograms
+import kotlinx.android.synthetic.main.activity_home.*
+
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        var currentTheme = MyPreferences(this).darkMode
+        applyTheme(currentTheme)
         val myprog= findViewById<TextView>(R.id.MyPrograms)
         val clickscanrun = findViewById<TextView>(R.id.clickscanrun)
         val notes = findViewById<TextView>(R.id.notes)
+        val themeSwitch = findViewById<ImageButton>(R.id.themeSwitch)
 
         myprog.setOnClickListener {
             var intent = Intent( this, MyPrograms::class.java)
@@ -29,6 +37,33 @@ class Home : AppCompatActivity() {
             var intent = Intent( this, Notes::class.java)
             startActivity(intent)
         }
+        themeSwitch.setOnClickListener {
+            if(currentTheme == AppCompatDelegate.MODE_NIGHT_NO){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                delegate.applyDayNight()
+                MyPreferences(this).darkMode = AppCompatDelegate.MODE_NIGHT_YES
+                themeSwitch.setImageResource(R.drawable.bulboff)
 
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                delegate.applyDayNight()
+                MyPreferences(this).darkMode = AppCompatDelegate.MODE_NIGHT_NO
+                themeSwitch.setImageResource(R.drawable.bulbon)
+
+            }
+        }
+
+    }
+    private fun applyTheme(theme:Int){
+        AppCompatDelegate.setDefaultNightMode(theme)
+        delegate.applyDayNight()
+        if(theme == AppCompatDelegate.MODE_NIGHT_NO){
+            themeSwitch.setImageResource(R.drawable.bulbon)
+        }
+        else{
+            themeSwitch.setImageResource(R.drawable.bulboff)
+
+        }
     }
 }
---
+++ CodeY/app/src/main/java/com/isavit/codey/home/MyPreferences.kt
@@ -0,0 +1,17 @@
+package com.isavit.codey.home
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+
+class MyPreferences(context: Context?) {
+    companion object {
+        private const val DARK_STATUS = "com.isavit.codey.DARK_STATUS"
+    }
+
+    private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    var darkMode = preferences.getInt(DARK_STATUS, AppCompatDelegate.getDefaultNightMode())
+        set(value) = preferences.edit().putInt(DARK_STATUS, value).apply()
+
+}
